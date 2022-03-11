c48b9ecdb57907d60d97e869a81220bbc5b1c5aa
unknown <antonioarjpi@gmail.com>
2022-03-10 23:19:10 -0300

    Primeiro commit

---
+++ src/main/java/com/devsimple/brasileiraodigital/BrasileiraodigitalApplication.java
@@ -0,0 +1,13 @@
+package com.devsimple.brasileiraodigital;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrasileiraodigitalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrasileiraodigitalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/devsimple/brasileiraodigital/BrasileiraodigitalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devsimple.brasileiraodigital;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrasileiraodigitalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
